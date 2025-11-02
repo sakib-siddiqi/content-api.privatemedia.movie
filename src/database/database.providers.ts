edc1d14bdf9987da81fe8d343953a5593d63a1f6
sakib-siddiqi <sakibsiddiqi15@gmail.com>
2025-11-02 21:44:10 +0600

    supabase uncomment

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -7,21 +7,21 @@ export const databaseProviders = [
     provide: 'SEQUELIZE',
     inject: [ConfigService],
     useFactory: async (configService: ConfigService) => {
-      // const db_host = new URL(configService.get<string>('SUPABASE_PROJECT_URL') || '').host;
-      // if (!db_host) {
-      //   throw new Error('DATABASE URL is not defined in environment variables');
-      // }
+      const db_host = new URL(configService.get<string>('SUPABASE_PROJECT_URL') || '').host;
+      if (!db_host) {
+        throw new Error('DATABASE URL is not defined in environment variables');
+      }
 
-      // const sequelize = new Sequelize('postgresql://postgres.brkhkpwapsbxoavdghjv:JKk6iMKUhK4A3PJh@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres',{
-      //   dialect: 'postgres',
-      //   password: configService.get<string>('SUPABASE_DB_PASSWORD'),
-      //   logging: false,
-      //   models: [Cache],
-      // });
+      const sequelize = new Sequelize('postgresql://postgres.brkhkpwapsbxoavdghjv:JKk6iMKUhK4A3PJh@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres',{
+        dialect: 'postgres',
+        password: configService.get<string>('SUPABASE_DB_PASSWORD'),
+        logging: false,
+        models: [Cache],
+      });
 
-      // await sequelize.authenticate();
-      // console.log('✅ Connected to Supabase Postgres successfully');
-      // return sequelize;
+      await sequelize.authenticate();
+      console.log('✅ Connected to Supabase Postgres successfully');
+      return sequelize;
     },
   },
 ];
